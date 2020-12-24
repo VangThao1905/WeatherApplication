20248210245fbfaef98f9c1969bde02fbff48cf5
VangThao1905 <vangthao1905@gmail.com>
2020-12-24 10:20:37 +0700

    Delete class SharedPreferencesConfig.java

--- app/src/main/java/vangthao/app/weatherapplication/data/SharedPreferencesConfig.java
+++
@@ -1,8 +0,0 @@
-package vangthao.app.weatherapplication.data;
-
-import android.content.SharedPreferences;
-
-public class SharedPreferencesConfig {
-    public static SharedPreferences sharedPreferences;
-    public static SharedPreferences.Editor editor;
-}
